0bcb305fb438441eca6dbeaf3fde933cc0ae9905
Fedja <fedja.hadziselimovic@gmail.com>
2016-04-12 14:32:17 +0200

    Fifth Commit /2

--- server/config/seed.js
+++ server/config/seed.js
@@ -10,22 +10,22 @@ var User = require('../api/user/user.model');
 
 Thing.find({}).remove(function() {
   Thing.create({
-    name : 'Development Tools',
+    name : 'Gaming Consoles',
     info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
   }, {
-    name : 'Server and Client integration',
+    name : 'Arcade Machines',
     info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
   }, {
-    name : 'Smart Build System',
+    name : 'Dart Machines',
     info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
   },  {
-    name : 'Modular Structure',
+    name : 'Gaming Computers',
     info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
   },  {
-    name : 'Optimized Build',
+    name : 'Billiard Tables',
     info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
   },{
-    name : 'Deployment Ready',
+    name : 'Table Football',
     info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
   });
 });
@@ -36,8 +36,34 @@ User.find({}).remove(function() {
     name: 'Test User',
     email: 'test@test.com',
     password: 'test'
-  }, {
-    provider: 'local',
+  },
+
+  { provider: 'local',
+    role: 'User',
+    name: 'Damir',
+    email: 'damir@aubih.edu',
+    password: 'damir',
+    },
+    { provider: 'local',
+      role: 'User',
+      name: 'Emir',
+      email: 'emir@aubih.edu',
+      password: 'emir',
+      },
+      { provider: 'local',
+        role: 'User',
+        name: 'Amra',
+        email: 'amra@aubih.edu',
+        password: 'amra',
+        },
+        { provider: 'local',
+          role: 'User',
+          name: 'Adi',
+          email: 'adi@aubih.edu',
+          password: 'adi',
+          },
+
+  {  provider: 'local',
     role: 'admin',
     name: 'Admin',
     email: 'admin@admin.com',
